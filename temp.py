8fc239cc22f87b8016a0e02725114bdea6f8af98
DamChu <chdamian@o2.pl>
2025-09-16 12:05:54 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,18 +1,1 @@
-# Plik tymczasowy: update_mock_passwords.py
-from auth import hash_password
 
-# Hasła, które chcemy nadać naszym mockowym użytkownikom
-mock_users = {
-    'wedkarz_1': 'adam123',
-    'rybak_stefan': 'ewa456',
-    'anna_fish': 'janusz789',
-    'wielki_fiszer': 'maupiszon123'
-}
-
-print("-- Skopiuj i wykonaj poniższe polecenia SQL w VS Code, aby zaktualizować hasła:")
-for username, password in mock_users.items():
-    hashed = hash_password(password)
-    # Generujemy gotowe polecenie UPDATE
-    print(f"UPDATE Users SET password_hash = '{hashed}' WHERE username = '{username}';")
-
-print("COMMIT;")
